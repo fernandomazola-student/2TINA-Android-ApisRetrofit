f07d58029471b9169bc695001e417f6a2480ee70
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-12 10:38:26 -0300

    Instanciado retrofit na main activity, no metodo oncreate

--- app/src/main/java/br/com/fiap/a2tina_android_apisretrofit/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_apisretrofit/MainActivity.java
@@ -3,11 +3,23 @@ package br.com.fiap.a2tina_android_apisretrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://douglascabral.com.br/aula-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
     }
+
+
 }
