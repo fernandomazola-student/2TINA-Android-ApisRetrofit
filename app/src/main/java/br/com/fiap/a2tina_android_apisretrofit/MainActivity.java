bf256413976d5de85e65883574915d74d8b98674
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-12 10:53:44 -0300

    Criado no metodo onReponse da api, a logica para imprimir todos os dados da lista que vem da api

--- app/src/main/java/br/com/fiap/a2tina_android_apisretrofit/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_apisretrofit/MainActivity.java
@@ -2,7 +2,13 @@ package br.com.fiap.a2tina_android_apisretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -19,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         RetrofitContatosInterface api = retrofit.create(RetrofitContatosInterface.class);
+
+        api.getContatos().enqueue(new Callback<List<Contato>>() {
+            @Override
+            public void onResponse(Call<List<Contato>> call, Response<List<Contato>> response) {
+                List<Contato> contatos = response.body();
+                for ( int i = 0; i < contatos.size(); i++) {
+                    Log.i("CONTATO", contatos.get(i).getNome());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Contato>> call, Throwable t) {
+
+            }
+        });
     }
 
 
